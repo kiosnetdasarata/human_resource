15365eeed8d0b1daf7f4cbf041ec56b8075d8652
Wardah Zahra <103308361+wardahzet@users.noreply.github.com>
2023-09-22 14:42:07 +0700

    Update SalesService.php

--- app/Services/SalesService.php
+++ app/Services/SalesService.php
@@ -3,55 +3,53 @@
 namespace App\Services;
 
 use Ramsey\Uuid\Uuid;
-use Illuminate\Support\Str;
-use App\Interfaces\EmployeeRepositoryInterface;
-use App\Interfaces\JobTitleRepositoryInterface;
+use App\Interfaces\SalesRepositoryInterface;
 use App\Interfaces\StatusLevelRepositoryInterface;
 
-class EmployeeService
+class SalesService
 {
     public function __construct(
-        private EmployeeRepositoryInterface $employeeRepositoryInterface,
-        private JobTitleRepositoryInterface $jobTitleRepositoryInterface,
-        private StatusLevelRepositoryInterface $statusLevelRepositoryInterface,
-        )
-    {}
+        private SalesRepositoryInterface $salesRepositoryInterface,
+        private StatusLevelRepositoryInterface $statusLevelRepositoryInterface
+    )
+    {
+    }
 
     public function getAll()
     {
-        return $this->employeeRepositoryInterface->getAll();
+        return $this->salesRepositoryInterface->getAll();
     }
 
     public function find($uuid)
     {
-        return $this->employeeRepositoryInterface->find($uuid);
+        return $this->salesRepositoryInterface->find($uuid);
     }
 
     public function store($request)
     {
-        $sales = collect($request);
-        $sales->merge([
+        $sales = collect($request)->merge([
             'uuid' => Uuid::uuid4()->getHex(),
-            'level_id' => $this->statusLevelRepositoryInterface->getLevelByCommission($sales->komisi_id),
+            'level_id' => $this->statusLevelRepositoryInterface->getCommission($request['komisi_id'])->level_id,
         ]);
-        return $this->employeeRepositoryInterface->create($request);
+
+        return $this->salesRepositoryInterface->create($sales->all());
     }
 
     public function update($id, $request)
     {
         $old = $this->find($id);
-        $sales = collect($request);
+        $sales = collect($request->validate())->diffAssoc($old);
 
-        if (isset($sales->komisi_id) && $sales->komisi_id != $old->komisi_id) {
-            $sales->put('level_id', $this->statusLevelRepositoryInterface->getLevelByCommission($sales->komisi_id));
+        if (isset($sales['komisi_id'])) {
+            $sales->put('level_id', $this->statusLevelRepositoryInterface->getCommission($sales['komisi_id'])->level_id);
         }
         
-        return $this->employeeRepositoryInterface->update($old, $request);
+        return $this->salesRepositoryInterface->update($old, $sales->all());
     }
 
     public function delete($data)
     {
-        return $this->employeeRepositoryInterface->delete($data);
+        return $this->salesRepositoryInterface->delete($this->find($data));
     }
 }
 
