8ee4c5115b31cda2867852a058306adcf68e225e
Wardah Zahra <103308361+wardahzet@users.noreply.github.com>
2023-10-21 12:37:51 +0700

    Create JobVacancyController.php

---
+++ app/Http/Controllers/JobVacancyController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Interfaces\JobVacancyRepositoryInterface;
+
+class JobVacancyController extends Controller
+{
+    public function __construct(private JobVacancyRepositoryInterface $jobVacancy) 
+    {
+    }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        try{
+            return response()->json([
+                'status' => 'success',
+                'data' => $this->jobVacancy->getAll(),
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => 'error',
+                'error' => $e->getMessage(),
+                'status_code' => $e->getCode(),
+            ]);
+        }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
