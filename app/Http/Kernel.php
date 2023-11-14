b12962ff72a7213ac6cf67055d0e466211c00ac7
Wardah Zahra <103308361+wardahzet@users.noreply.github.com>
2023-11-14 11:40:40 +0700

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,5 +65,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'jwt' => \App\Http\Middleware\JwtMiddleware::class,
+        'is_human_resource' => \App\Http\Middleware\IsHumanResource::class,
     ];
 }
